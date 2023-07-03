d60d1191daa099ba2df73770caadf08f8a6a69e9
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-07-03 06:08:51 +0000

    [Update]新規登録機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,15 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  
+  protected
+  
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:name_id])
+  end
 
   # GET /resource/sign_in
   # def new
