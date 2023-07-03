de6718db3a162120d043a37e148699e73a1fb357
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-07-03 04:09:48 +0000

    [Update]新規登録画面

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,14 +4,11 @@ module.exports = environment
 
 
 const webpack = require('webpack')
-
 environment.plugins.prepend(
   'Provide',
-
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: '/jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
-
   })
-  )
+)
