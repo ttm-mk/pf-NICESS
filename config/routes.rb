c5798d267c0fef0977fab6c6b14070cf8a31bde7
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-07-05 01:23:09 +0000

    [Update]ルーティング配置修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,22 @@ Rails.application.routes.draw do
   root to: 'public/homes#top'
   get '/about' => 'public/homes#about'
 
+    # ゲストログイン用
+  devise_scope :user do
+    post 'user/guest_sign_in' => 'public/sessions#guest_sign_in'
+  end
+
+  # 管理者用
+  devise_for :admins,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # ユーザー用
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
   # admin
   namespace :admin do
     get '/' => 'homes#top'
@@ -17,21 +33,7 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :update]
   end
 
-  # 管理者用
-  devise_for :admins,skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
-  # ユーザー用
-  devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
   
-    # ゲストログイン用
-  devise_scope :user do
-    post 'user/guest_sign_in' => 'public/sessions#guest_sign_in'
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
