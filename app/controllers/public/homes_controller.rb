e22d01fd7877d01414dc3058023147aaee28bad4
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-07-11 02:38:41 +0000

    [Update]投稿検索機能

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,16 @@ class Public::HomesController < ApplicationController
   def top
     @post = Post.new
     @posts = Post.all
+    
+    if params[:search].present?
+      @posts = Post.where('text LIKE ?', "%#{params[:search]}%")
+    
+    elsif params[:category].present?
+      @category = Category.find(params[:name])
+
+    else
+      @posts = Post.all
+    end
   end
 
   def about
