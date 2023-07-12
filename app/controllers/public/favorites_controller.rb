8b3891fb2530c30707094226e0b14d6492d1f62a
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-07-12 03:27:50 +0000

    [Update]いいね機能

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,21 +1,21 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    @post = Post.find(params[:post_id])
-    favorite = Favorite.new(user_id: current_user.id, post_id: @post)
+    # @post = Post.find(params[:post_id])
+    favorite = Favorite.new(user_id: current_user.id, post_id: params[:post_id])
     if !favorite.save
       redirect_to root_path
     else
-      redirect_to user_post_path(params[:post_id])
+      redirect_to user_post_path(params[:user_id], params[:post_id])
     end
 
   end
 
   def destroy
     # @post = Post.find_by(id: params[:id])
-    favorite = Favorites.find_by(user_id: current_user.id, post_id: params[:id])
+    favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
     if favorite.destroy
-      redirect_to user_post_path(params[:post_id])
+      redirect_to user_post_path(params[:user_id], params[:post_id])
     else
       redirect_to root_path
     end
