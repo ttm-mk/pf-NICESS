c3793a87d960791daa17274a64d5d079fc1beabc
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-07-23 02:54:31 +0000

    [Add]cart_item

---
+++ db/migrate/20230723025002_create_cart_items.rb
@@ -0,0 +1,12 @@
+class CreateCartItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cart_items do |t|
+      
+      t.integer :amount, null: false
+      t.integer :user_id, null: false
+      t.integer :item_id, null: false
+
+      t.timestamps
+    end
+  end
+end
