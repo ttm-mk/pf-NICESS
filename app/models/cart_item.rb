fcc7c35e07abb27745b1466323d0100e6d20a855
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-08-24 15:27:54 +0000

    [Update]商品一覧レスポンシブ修正、注文詳細ステータス表示変更

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,7 +5,7 @@ class CartItem < ApplicationRecord
   belongs_to :item
 
   # バリデーション
-  # validates :amount, presence: true, numbericality: { only_integer: true, greater_than: 0 }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
 
 end
