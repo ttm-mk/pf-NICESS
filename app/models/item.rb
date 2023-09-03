a42affe2dfcda7625541fee173ab7765ce5c20e7
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-09-03 15:00:34 +0000

    [Update]itemモデル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   belongs_to :shop
   belongs_to :category
   has_many :cart_items, dependent: :destroy
-  has_many :order_details, dependent: :destroy
+  has_many :order_details
   has_many :orders, through: :order_details
 
   has_one_attached :item_image
